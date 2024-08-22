c01df6f0dd28d646b5a10f73b8fd1e3bfb8a9e3b
Akshat-Shah04 <shahakshat200@gmail.com>
2024-08-22 19:34:03 +0530

    Assignment Files

---
+++ Module 3/Q44UniqueValueOfDict.py
@@ -0,0 +1,21 @@
+# Write a Python program to print all unique values in a dictionary.
+d = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 1,
+    'd' : 3,
+    'e' : 1,
+    'f' : 10,
+    'g' : 5,
+    'h' : 3,
+    'i' : 6,
+}
+count = set()
+for val in d.values():
+    count.add(val)
+    
+print(count)
+
+'''
+{1, 2, 3, 5, 6, 10}
+'''
