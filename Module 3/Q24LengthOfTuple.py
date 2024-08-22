3ad2dd08594d451e562041089d5cef01d202daee
Akshat-Shah04 <shahakshat200@gmail.com>
2024-08-22 10:39:44 +0530

    Python Assignment Files

---
+++ Module 3/Q24LengthOfTuple.py
@@ -0,0 +1,8 @@
+# Write a Python program to find the length of a tuple
+t = (1,2,3,4,5,6,7,8,9,10)
+print(f"Length of the Tuple is {len(t)}")
+
+'''
+Length of the Tuple is 10
+'''
+
---
+++ Module 3/Q26ReverseTuple.py
@@ -0,0 +1,9 @@
+# Write a Python program to reverse a tuple
+t = (1,2,3,4,5)
+t1 = t[::-1]
+print("Original Tuple : ",t)
+print("Reversed Tuple : ",t1)
+'''
+Original Tuple :  (1, 2, 3, 4, 5)
+Reversed Tuple :  (5, 4, 3, 2, 1)
+'''
